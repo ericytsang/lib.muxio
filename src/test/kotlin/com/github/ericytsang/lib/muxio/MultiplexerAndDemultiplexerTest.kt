52e9259441f04f7b4b9d307a48adacded41ffeaa
Eric Tsang <surplus.et@gmail.com>
2016-10-28 02:29:53 -0700

    add test cases

--- src/test/kotlin/com/github/ericytsang/lib/muxio/MultiplexerAndDemultiplexerTest.kt
+++ src/test/kotlin/com/github/ericytsang/lib/muxio/MultiplexerAndDemultiplexerTest.kt
@@ -1,7 +1,5 @@
 package com.github.ericytsang.lib.muxio
 
-import com.github.ericytsang.lib.muxio.Demultiplexer
-import com.github.ericytsang.lib.muxio.Multiplexer
 import org.junit.Test
 import java.io.DataInputStream
 import java.io.PipedInputStream
@@ -14,29 +12,30 @@ import kotlin.concurrent.thread
  */
 class MultiplexerAndDemultiplexerTest
 {
+    val pipeOut = PipedOutputStream()
+    val pipeIn = PipedInputStream(pipeOut)
+    val mux = Multiplexer(pipeOut)
+    val demux = Demultiplexer(pipeIn)
+
     @Test
     fun testGeneral()
     {
         val dataToSend = setOf(byteArrayOf(1,1,1,1,1,1),byteArrayOf(1,3,3,3,1,1),byteArrayOf(1,1,4,4,4,4))
         val dataReceived = setOf(ByteArray(6),ByteArray(6),ByteArray(6))
-        val pipedIns = PipedInputStream(1)
-        val pipedOuts = PipedOutputStream(pipedIns)
-        val demux = Demultiplexer(pipedIns)
-        val mux = Multiplexer(pipedOuts)
 
         val t1 = thread()
         {
             val connections = dataToSend.associate {it to mux.connect()}
             connections.forEach {it.value.write(it.key)}
             connections.forEach {println("closing");it.value.close();println("closed");}
-            pipedOuts.close()
+            pipeOut.close()
         }
         val t2 = thread()
         {
             val connections = dataReceived.associate {it to demux.accept()}
             connections.forEach {DataInputStream(it.value).readFully(it.key)}
             connections.forEach {it.value.close()}
-            pipedIns.close()
+            pipeIn.close()
             println(dataReceived.map {Arrays.toString(it)})
         }
         t1.join()
@@ -44,4 +43,26 @@ class MultiplexerAndDemultiplexerTest
 
         assert(dataReceived.map {Arrays.toString(it)}.toSet() == dataToSend.map {Arrays.toString(it)}.toSet())
     }
+
+    @Test
+    fun connectSendCloseThenAcceptAndRead()
+    {
+        val dataSend = byteArrayOf(1,1,1,1,1,1)
+        val dataRecv = ByteArray(dataSend.size)
+        mux.connect().use {it.write(dataSend)}
+        demux.accept().let(::DataInputStream).use {it.readFully(dataRecv)}
+        assert(Arrays.equals(dataSend,dataRecv))
+    }
+
+    fun convertByteTest(payload:Int)
+    {
+        mux.connect().write(payload)
+        val int:Int = demux.accept().let(::DataInputStream).read()
+        assert(int == payload)
+    }
+
+    @Test fun convertingByteTest1() = convertByteTest(255)
+    @Test fun convertingByteTest2() = convertByteTest(254)
+    @Test fun convertingByteTest3() = convertByteTest(5)
+    @Test fun convertingByteTest4() = convertByteTest(Byte.MAX_VALUE.toInt())
 }
